9a72ba2fa3dfee9542ebf995d223d655020bbcc1
Robert Szalamin <szalamin.robert@gmail.com>
2022-04-11 16:49:31 +0200

    1st

---
+++ eas.js
@@ -0,0 +1,9 @@
+function soknegyzet() {
+    let hanydb = prompt("Hány négyzet legyen egy sorban?");
+}
+
+const halozat = document.createElement("div");
+
+halozat.textContent = "WTF";
+
+grid.appendChild(halozat);
